91a02c5d5ed30427543fb62d6e3d017ec0c87003
Charles Muhanji <142399450+Charles13064@users.noreply.github.com>
2025-09-12 22:52:57 +0300

    Add files via upload

---
+++ Class assignment.c
@@ -0,0 +1,16 @@
+/*Name: Charles Muhanji
+Registration No: PA106/G/28735/25
+Description: A program Showing units to be covered by Software Engineering First Year Students at KyU 2025 and their respective codes.
+*/
+#include <stdio.h>
+int main() {
+    printf("Software Engineering First Year Students at KyU 2025 and their respective unit codes:\n");
+    printf("1. Computer System and Organisations - SPI 2102\n");
+    printf("2. Introduction to Programming and Problem Solving- SPC 2101\n");
+    printf("3. Discrete Mathematics- SPM 2100\n");
+    printf("4. Fundamentals of ICT- UCU 2105\n");
+    printf("5. Communication Skills- UCU 2101\n");
+    printf("6. Mathematics for Science- SPM 2104\n");
+    printf("7. Fundamentals of System Analysis and Design- SSE 2101\n");
+    return 0;
+}
